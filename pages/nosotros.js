8895e318b8097115c4e83f04d0230c8e2e4860b5
GastonCorimayo <42283804+gec012@users.noreply.github.com>
2023-02-25 02:39:46 -0300

    primer commit nextjs

---
+++ pages/nosotros.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+import Link from "next/link";
+export default function Nosotros(){
+    return (
+        <Layout>
+          
+            <Link href="/">Ir a Inicio</Link>
+        </Layout>
+    )
+}
